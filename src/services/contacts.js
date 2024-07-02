4e0dee33342f3f1c3b67e835d8a95309c53e5395
AnastasiiaV0304 <volosina608@gmail.com>
2024-07-02 11:18:32 +0300

    new

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -46,11 +46,10 @@ export const getContactById = async (contactId, userId) => {
 };
 
 export const createContact = async (payload, userId) => {
-  const contactData = {
+  const contact = await ContactsCollection.create({
+    userId,
     ...payload,
-    userId: userId,
-  };
-  const contact = await ContactsCollection.create(contactData);
+  });
   return contact;
 };
 
@@ -62,14 +61,9 @@ export const deleteContact = async (contactId, userId) => {
   return contact;
 };
 
-export const updateContact = async (
-  contactId,
-  payload,
-  userId,
-  options = {},
-) => {
+export const updateContact = async (contactId, payload, options = {}) => {
   const rawResult = await ContactsCollection.findOneAndUpdate(
-    { _id: contactId, userId },
+    { _id: contactId, userId: payload.userId },
     payload,
     {
       new: true,
